0fefe6654a320ebb77dcf715146bc6ddf7c0ad42
Viviane <viviane.sm3@gmail.com>
2026-05-17 15:17:32 -0400

    Add accordion functionality

--- setup.js
+++ setup.js
@@ -36,3 +36,23 @@ function displayDeck(list) {
       }
       return string;
 }
+
+/* Code from https://www.w3schools.com/howto/howto_js_accordion.asp */
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+} 
